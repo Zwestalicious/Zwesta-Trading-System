fdbd62eeb8b56ad4f18912a4ae9abbef045d7cba
Builder <builder@zwesta.com>
2026-08-22 01:26:39 +0200

    Remove temporary test files from git tracking

--- check_creds.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn = psycopg2.connect('postgresql://zwesta_admin:Zwesta%40Trading2026!@localhost:5432/zwesta_trading')
-cur = conn.cursor()
-cur.execute("SELECT credential_id, user_id, broker_name, is_live, account_number, server FROM broker_credentials WHERE broker_name ILIKE '%exness%' LIMIT 5")
-for row in cur.fetchall():
-    print(row)
-conn.close()
